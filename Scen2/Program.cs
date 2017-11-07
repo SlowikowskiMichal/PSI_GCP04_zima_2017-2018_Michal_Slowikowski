c0e0925876d6048c902c5209aa2d9acdd9515068
slovi <slovik421@gmail.com>
2017-11-07 22:42:40 +0100

    input data

--- Scen2/Program.cs
+++ Scen2/Program.cs
@@ -9,7 +9,12 @@ namespace Scen2
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            int[] result = Letters.getLetter(1,2);
+
+            Console.WriteLine(result[0] + "|" + result[1] + "|" + result[2] + "\n" + 
+                              result[3] + "|" + result[4] + "|" + result[5]);
+            Console.ReadLine();
         }
     }
 }
