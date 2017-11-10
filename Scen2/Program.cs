2f92477ecd6f11cdda8729e5d946874b8847de00
slovi <slovik421@gmail.com>
2017-11-10 03:36:13 +0100

    added log system

--- Scen2/Program.cs
+++ Scen2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,21 +11,40 @@ namespace Scen2
     {
         static void Main(string[] args)
         {
+            double learningRate = 0.001;
+
+            FileStream ostrm;
+            StreamWriter writer;
+            TextWriter oldOut = Console.Out;
+
+            try
+            {
+                ostrm = new FileStream("X" + Letters.NumberOfFieldsX +"Y" + Letters.NumberOfFieldsY + "_" + learningRate + "Log.txt", FileMode.Create, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open " + Letters.NumberOfFieldsX + Letters.NumberOfFieldsY + "_" + learningRate + "Log.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.SetOut(writer);
             Console.WriteLine("Number of Width Fields: " + Letters.NumberOfFieldsX + " Number of Height Fields: " + Letters.NumberOfFieldsY);
 
             Console.WriteLine("-------------------------Perceptron----------------------");
-            PercetronTrainer trainer = new PercetronTrainer(Letters.NumberOfFields, 0.1);
+            PercetronTrainer trainer = new PercetronTrainer(Letters.NumberOfFields, learningRate);
             trainer.Train();
             trainer.Test(Letters.LettersData);
             trainer.Test(Letters.CorruptedLettersData);
 
             Console.WriteLine("-------------------------Adaline----------------------");
-            AdalineTrainer trainerA = new AdalineTrainer(Letters.NumberOfFields, 0.1);
+            AdalineTrainer trainerA = new AdalineTrainer(Letters.NumberOfFields, learningRate);
             trainerA.Train();
             trainerA.Test(Letters.LettersData);
             trainerA.Test(Letters.CorruptedLettersData);
 
-            Console.ReadLine();
+            writer.Close();
+            ostrm.Close();
         }
     }
 }
